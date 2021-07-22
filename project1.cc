20fc9ea9a88306e47dc93a5fdbb0c221e631eb86
neeharika25-star <82817797+neeharika25-star@users.noreply.github.com>
2021-07-22 22:47:48 +0530

    project1

---
+++ project1.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout<<sizeof(int)<<" byte size of int"<<endl;
+   cout<<sizeof(float)<<" byte size of float"<<endl;
+   cout<<sizeof(double)<<" byte sizo of double"<<endl;
+   cout<<sizeof(char)<<" byte size of char"<<endl;
+   return 0;
+}
